4a072982b3657bfa8e6ee6d7084544b27b22c106
akhtarmuzaqie <46335645+akhtarmuzaqie@users.noreply.github.com>
2019-01-28 13:57:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Betulindong | Service</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Vegur_500.font.js" type="text/javascript"></script>
+<script src="js/FF-cash.js" type="text/javascript"></script>
+<script src="js/tms-0.3.js" type="text/javascript"></script>
+<script src="js/tms_presets.js" type="text/javascript"></script>
+<script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="js/jquery.equalheights.js" type="text/javascript"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+<body id="page1">
+ <div id="loading">
+            <div id="loading-center">
+                <div id="loading-center-absolute">
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                    <div class="object"></div>
+                </div>
+            </div>
+        </div>
+
+<div class="main-bg">
+  <div class="bg">
+    <!--==============================header=================================-->
+    <header>
+      <div class="main">
+        <div class="wrapper">
+          <h1><a href="index.html">Betulindong</a></h1>
+          <div class="fright">
+            <div class="indent">
+              <span class="address">Jl. Danau Ranau, Sawojajar, Kedungkandang, Kota Malang, Jawa Timur 65139</span>
+              <span class="instagram">Instagram : @betulindong.com</span>
+              <span> | </span>
+              <span class="youtube">Youtube : Betulindong Official</span>
+            </div>
+          </div>
+        </div>
+        <nav>
+          <ul class="menu">
+            <li><a class="active" href="index.html">Menu</a></li>
+            <li><a href="about.html">Tentang Kami</a></li>
+            <li><a href="price.html">Harga</a></li>
+            <li><a href="locations.html">Kirim Masalah Anda</a></li>
+          </ul>
+        </nav>
+        <div class="slider-wrapper">
+          <div class="slider">
+            <ul class="items">
+              <li> <img src="images/slider-img1.jpg" alt="" /> </li>
+              <li> <img src="images/slider-img2.jpg" alt="" /> </li>
+              <li> <img src="images/slider-img3.jpg" alt="" /> </li>
+            </ul>
+          </div>
+          <a class="prev" href="#">prev</a> <a class="next" href="#">next</a> </div>
+      </div>
+    </header>
+    <!--==============================content================================-->
+    <section id="content">
+      <div class="main">
+        <div class="container_12">
+          <div class="wrapper p5">
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="images/page1-img1.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Aman</h4>
+                  <p class="p2">Tukang-Tukang kami dipilih melalui proses seleksi dan verifikasi ketat. Sistem kami dilengkapi dengan kode pengaman unik Tukang untuk setiap order pengerjaan.</p>
+              </div>
+            </article>
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="images/page1-img2.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Hemat</h4>
+                  <p class="p2">Detail jenis layanan dan tarif masing-masing Tukang tersedia. Aplikasi kami juga memungkinkan customer untuk berkomunikasi guna mendapatkan jenis layanan yang dibutuhkan dengan budget yang sesuai. </p>
+              </div>
+            </article>
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="images/page1-img3.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Cepat</h4>
+                  <p class="p2">Sistem kami memungkinkan customer dan Tukang untuk mengatur jadwal pengerjaan.</p>
+              </div>
+            </article>
+          </div>
+          <div class="container-bot">
+            <div class="container-top">
+              <div class="container">
+                <div class="wrapper">
+                  <article class="grid_8">
+                    <div class="indent-left">
+                      <h2>Selamat Datang!</h2>
+                      <p class="prev-indent-bot"><strong>Betulindong</strong> adalah aplikasi untuk membantu anda membenarkan PC anda apapun masalahnya.</p>
+                      <p class="border-bot">Aplikasi ini dibuat oleh siswa SMK Telkom Malang</p>
+                    </div>
+                    <div class="wrapper">
+                      <div class="grid_4 alpha">
+                        <div class="indent-left">
+                          <div class="maxheight indent-bot">
+                            <h3>Tentang Kami</h3>
+                            <p class="prev-indent-bot"><a class="link-1" href="#">SMK Telkom Malang</a> pelopor Sekolah menengah kejuruan pertama di Indonesia di bidang Teknologi dan Informatika. </p>
+                            <a class="link-1" href="#">Quadralion</a> adalah nama tim yang membuat aplikasi ini. Kami berempat adalah siswa kelas 10 RPL 3.</div>
+                          </div>
+                      </div>
+                      <div class="grid_4 omega">
+                        <div class="indent-left2">
+                          <div class="maxheight indent-bot">
+                            <h3 class="p0">Melayani</h3>
+                            <ul class="list-1">
+                              <li><a href="#">Install Laptop</a></li>
+                              <li><a href="#">Install Windows</a></li>
+                              <li><a href="#">Install Software Standar</a></li>
+                              <li><a href="#">Pemasangan Hardware</a></li>
+                              <li><a href="#">Backup dan Recovery Data</a></li>
+                              <li><a href="#">Perakitan PC</a></li>
+                            </ul>
+                          </div>
+                      </div>
+                    </div>
+                  </article>
+                  <article class="grid_4">
+                    <div class="indent-left2 indent-top">
+                      <div class="box p4">
+                        <div class="padding">
+                          <div class="wrapper">
+                            <figure class="img-indent"><img src="images/page1-img4.png" alt=""></figure>
+                            <div class="extra-wrap">
+                              <h3 class="p0">Jam Kerja :</h3>
+                            </div>
+                          </div>
+                          <p class="pp"><strong>Menerima Panggilan 24 Jam</strong></p>
+                          <p class="color-1 p0">Senin - Jumat : 06.15 - 15.30</p>
+                          <p class="color-1 p1">Sabtu : 08.00 - 10.00</p></div>
+                      </div>
+                      <div class="indent-left">
+                        <dl class="main-address">
+                          <dt>Jl. Danau Ranau, Sawojajar, Kedungkandang, Kota Malang, Jawa Timur 65139</dt>
+                          <dd><span>Telephone:</span> (0341) 712500;</dd>
+                          <dd><span>E-mail:</span><a>betulindong@gmail.com</a></dd>
+                        </dl>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--==============================footer=================================-->
+    <footer>
+      <div class="main"> <span>Copyright &copy; <a href="#">Domain Name</a> All Rights Reserved</span> Design by <a target="_blank" href="http://www.templatemonster.com/">TemplateMonster.com</a> </div>
+    </footer>
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+<script type="text/javascript">
+$(window).load(function () {
+    $('.slider')._TMS({
+        duration: 1000,
+        easing: 'easeOutQuint',
+        preset: 'simpleFade',
+        slideshow: 7000,
+        banners: false,
+        pauseOnHover: true,
+        pagination: false,
+        pagNums: false,
+        nextBu: '.next',
+        prevBu: '.prev'
+    });
+});
+</script>
+</body>
+</html>
---
+++ price.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Betulindong | Service</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Vegur_500.font.js" type="text/javascript"></script>
+<script src="js/FF-cash.js" type="text/javascript"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+<body id="page5">
+<div class="main-bg">
+  <div class="bg">
+    <!--==============================header=================================-->
+    <header>
+      <div class="main">
+        <div class="wrapper">
+          <h1><a href="index.html">Betulindong</a></h1>
+          <div class="fright">
+            <div class="indent">
+              <span class="address">Jl. Danau Ranau, Sawojajar, Kedungkandang, Kota Malang, Jawa Timur 65139</span>
+              <span class="instagram">Instagram : @betulindong.com</span>
+              <span> | </span>
+              <span class="youtube">Youtube : Betulindong Official</span>
+            </div>
+          </div>
+        </div>
+        <nav>
+          <ul class="menu">
+            <li><a class="active" href="index.html">Menu</a></li>
+            <li><a href="about.html">Tentang Kami</a></li>
+            <li><a href="price.html">Harga</a></li>
+            <li><a href="locations.html">Kirim Masalah Anda</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!--==============================content================================-->
+    <section id="content">
+      <div class="main">
+        <div class="container_12">
+          <div class="wrapper p5">
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="images/page1-img1.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Aman</h4>
+                  <p class="p2">Tukang-Tukang kami dipilih melalui proses seleksi dan verifikasi ketat. Sistem kami dilengkapi dengan kode pengaman unik Tukang untuk setiap order pengerjaan.</p>
+              </div>
+            </article>
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="images/page1-img2.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Hemat</h4>
+                  <p class="p2">Detail jenis layanan dan tarif masing-masing Tukang tersedia. Aplikasi kami juga memungkinkan customer untuk berkomunikasi guna mendapatkan jenis layanan yang dibutuhkan dengan budget yang sesuai. </p>
+              </div>
+            </article>
+            <article class="grid_4">
+              <div class="wrapper">
+                <figure class="img-indent"><img src="images/page1-img3.png" alt=""></figure>
+                <div class="extra-wrap">
+                  <h4>Cepat</h4>
+                  <p class="p2">Sistem kami memungkinkan customer dan Tukang untuk mengatur jadwal pengerjaan.</p>
+              </div>
+            </article>
+          </div>
+          <div class="container-bot">
+            <div class="container-top">
+              <div class="container">
+                <div class="wrapper">
+                  <article class="grid_4">
+                    <div class="indent-left">
+                      <h3>Yang Paling Populer:v</h3>
+                      <h6><a class="link color-1" href="#">Produk 1</a></h6>
+                      <p class="p3">apaan gitu</p>
+                      <h6><a class="link color-1" href="#">Produk 2</a></h6>
+                      <p class="img-indent-bot">gatau apa</p>
+                      <h6><a class="link color-1" href="#">Produk 3</a></h6>
+                      <p class="img-indent-bot">apaya</p>
+                      <h6><a class="link color-1" href="#">Produk 4</a></h6>
+                      iya heem</div>
+                  </article>
+                  <article class="grid_8">
+                    <div class="indent-left2">
+                      <h3 class="p1">List Harga</h3>
+                      <table class="price-list">
+                        <thead>
+                          <tr>
+                            <td>&nbsp;</td>
+                            <td>a</td>
+                            <td>b</td>
+                            <td>c</td>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>aa</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ab</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ac</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ad</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ae</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>af</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ag</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ah</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ai</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>aj</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>ak</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>al</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>am</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                          <tr>
+                            <td>an</td>
+                            <td>ooo</td>
+                            <td>Rp.0;</td>
+                            <td>Rp.0;</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--==============================footer=================================-->
+    <footer>
+      <div class="main"> <span>Copyright &copy; <a href="#">Domain Name</a> All Rights Reserved</span> Design by <a target="_blank" href="http://www.templatemonster.com/">TemplateMonster.com</a> </div>
+    </footer>
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
